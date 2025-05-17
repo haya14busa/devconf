1e54439ac216711d19902cba8aba22fdc0e7ca21
haya14busa <haya14busa@gmail.com>
2025-05-18 03:13:18 +0900

    support image preview for yabai_focus command

--- bin/yabai_focus.sh
+++ bin/yabai_focus.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -e
-RECENT_SPACE_TYPE=$(yabai -m query --spaces --space | jq -r .type)
-yabai -m space --layout float
-yabai -m signal --add event=window_created label=___FOCUS_FZF___ app=Ghostty \
-  action='yabai -m window --focus $YABAI_WINDOW_ID --toggle float --grid 5:5:1:1:3:3 && yabai -m signal --remove ___FOCUS_FZF___'
-open -na Ghostty --args --title="___FOCUS_FZF___" -e "${HOME}/bin/yabai_focus_fzf.sh"
-sleep 0.1
-yabai -m space --layout "$RECENT_SPACE_TYPE"
+TERMINAL_APP='Ghostty'
+yabai -m rule --add --one-shot app="^${TERMINAL_APP}$" manage=off grid=9:8:1:1:6:7
+open -na "$TERMINAL_APP" --args --title="___FOCUS_FZF___" -e "${HOME}/bin/yabai_focus_fzf.sh"
--- bin/yabai_focus_fzf.sh
+++ bin/yabai_focus_fzf.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-selected=$(yabai -m query --windows | jq -r '.[] | "\(.id)\t\(.app)\t\(.title)"' |
+TMPDIR=$(mktemp -d)
+trap 'rm -rf -- "$TMPDIR"' EXIT HUP INT TERM
+
+selected=$(yabai -m query --windows | jq -r '.[] | "\(.id)\tspace=\(.space)\t\(.app)\t\(.title)"' |
            column -t -s $'\t' |
-           gum filter --fuzzy |
+           fzf --header="ID SCREEN APP TITLE" --layout=reverse --preview-window=down:80% --preview='(){
+             window_id={1}
+             screencapture -x -l $window_id '$TMPDIR'/$window_id.png
+             dim=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}
+             kitten icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place="$dim@0x0" '$TMPDIR'/$window_id.png
+           }' |
            awk '{print $1}')
-           # fzf --header="ID APP TITLE" |
 
 if [[ -n "$selected" ]]; then
     yabai -m window --focus "$selected"
