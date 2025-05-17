9ed170526e3689d7df2f30829b4b143e7383d62a
haya14busa <haya14busa@gmail.com>
2025-05-17 23:53:03 +0900

    use gum filter instead of fzf

--- bin/yabai_focus_fzf.sh
+++ bin/yabai_focus_fzf.sh
@@ -2,8 +2,9 @@
 
 selected=$(yabai -m query --windows | jq -r '.[] | "\(.id)\t\(.app)\t\(.title)"' |
            column -t -s $'\t' |
-           fzf --header="ID APP TITLE" |
+           gum filter --fuzzy |
            awk '{print $1}')
+           # fzf --header="ID APP TITLE" |
 
 if [[ -n "$selected" ]]; then
     yabai -m window --focus "$selected"
